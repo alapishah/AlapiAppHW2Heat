c3e779e8184e681d864808500cd91f8e74c29d1a
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 13:49:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var map = L.map('mapid').setView([37.7749, -122.4194], 13);
 
-L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
-	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 22,
+	maxZoom: 20,
+	ext: 'png'
 }).addTo(map);
 
   // load GeoJSON from an external file
