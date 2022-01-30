00535433852162a46062c23f18d5f93bc8fb34f1
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 13:51:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var map = L.map('mapid').setView([37.7749, -122.4194], 13);
-
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 	subdomains: 'abcd',
-	minZoom: 0,
 	maxZoom: 20,
 	ext: 'png'
 }).addTo(map);
