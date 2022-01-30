c5b29585c9f096fbf221c6f8de6b8f7bc668b73f
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 13:40:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var map = L.map('mapid').setView([37.7749, -122.4194], 13);
 
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
-ext: 'png'
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
   // load GeoJSON from an external file
