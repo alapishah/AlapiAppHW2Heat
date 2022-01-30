f4f7190eed38141877d7e26b71417c06b022655f
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 13:47:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ L.tileLayer('https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-toke
 	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	minZoom: 0,
 	maxZoom: 22,
-	subdomains: 'abcd',
-	accessToken: '<your accessToken>'
 }).addTo(map);
 
   // load GeoJSON from an external file
