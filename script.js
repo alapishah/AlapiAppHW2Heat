0611847b57099fd198e20a36f16adc3caf18bea5
Alapi <59810553+alapishah@users.noreply.github.com>
2022-01-30 13:54:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var map = L.map('mapid').setView([37.7749, -122.4194], 13);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
+	minZoom: 0,
 	maxZoom: 20,
 	ext: 'png'
 }).addTo(map);
+
 
   // load GeoJSON from an external file
 $.getJSON('https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%201%20-%20GitHub%20and%20Leaflet/sf_crime.geojson', function(data) {
